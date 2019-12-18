3d160edb85246c9792ad7261cb4a7b9b46b5c2d6
muondu <munenemuondu@gmail.com>
2019-12-18 13:00:31 +0300

    I have put an input in the int

--- loop1.py
+++ loop1.py
@@ -1,8 +1,9 @@
 channels = {
-    "Bickolodean" : "Thundermans"
+    "Bickolodean" : "Thundermans",
     "Boomerang" : "Tom and Jerry",
     "Disney XD" : "Lab Rats"
 }
 print("Channels")
 
-input1 = input("How many things do you want:  ")
+input1 = int(input("How many things do you want:  "))
+
