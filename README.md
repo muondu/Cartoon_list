64c6494cf6cb377690c591cc900d0192445ccd70
muondu <munenemuondu@gmail.com>
2019-12-20 10:48:45 +0300

    Check out my README

---
+++ README.md
@@ -0,0 +1,8 @@
+# My cartoon
+
+This website will show you a list of cartoons
+Then it will ask you how many times do you want to print it and then if you say 5 it will ask you five times which cartoon you want
+
+
+
+Then it will show your favorite show
--- loop1.py
+++ loop1.py
@@ -1,11 +1,22 @@
 channels = {
-    "Bickolodean" : "Thundermans",
-    "Boomerang" : "Tom and Jerry",
-    "Disney XD" : "Lab Rats"
+    "Nickolodean" : 1,
+    "Boomerang" : 2,
+    "Disney XD" : 3,
 }
 print(channels)
-input1 = input("How many things do you want:  ")   
+input1 = input("How many times do you want it to print:  ")  
+c = int(input1)
 constructor = list()
 
-for h in input1:
-    print("Bye")
+for h in range(c):
+    input2 = channels[input("Enter what you want:  ")]
+    print("The show is " + str(input2))
+    
+    
+    constructor.append(input2)
+    
+    like = sum(constructor)
+        
+        
+        
+print("Your favorite show is " + str(like))
